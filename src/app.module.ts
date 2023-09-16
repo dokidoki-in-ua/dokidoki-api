c757d04424f09a8f7d5071c2a1248d8b348423b9
Mykyta Kasianenko <kasianenkomykyta@gmail.com>
2023-09-16 12:46:30 +0300

    feat(graphql): added Apollo GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CatalogModule } from './catalog/catalog.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [CatalogModule],
+  imports: [
+    CatalogModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
